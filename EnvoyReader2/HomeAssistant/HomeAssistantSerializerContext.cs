361c5033f18e56fa32a58e2f070ebcef6c7175d8
Jelmer Vos <43109366+jelmervos@users.noreply.github.com>
2026-09-08 15:27:59 +0200

    Add HomeAssistantSerializerContext

---
+++ EnvoyReader2/HomeAssistant/HomeAssistantSerializerContext.cs
@@ -0,0 +1,7 @@
+﻿using System.Text.Json.Serialization;
+
+[JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
+[JsonSerializable(typeof(State))]
+internal partial class HomeAssistantSerializerContext : JsonSerializerContext
+{
+}
--- EnvoyReader2/HomeAssistant/HomeAssistantServiceExtensions.cs
+++ EnvoyReader2/HomeAssistant/HomeAssistantServiceExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Refit;
+using System.Text.Json;
 
 internal static class HomeAssistantServiceExtensions
 {
@@ -10,9 +11,16 @@ internal static class HomeAssistantServiceExtensions
         var homeAssistantSettings = provider.GetRequiredService<IOptions<HomeAssistantSettings>>();
         services.AddRefitGeneratedClient<IHomeAssistantApi>(new RefitSettings
         {
+            ContentSerializer = GetContentSerializer(),
             AuthorizationHeaderValueGetter = (request, cancelToken) => ValueTask.FromResult(homeAssistantSettings.Value.Token)
         }).ConfigureHttpClient(c => c.BaseAddress = new Uri(homeAssistantSettings.Value.Address));
 
         return services;
     }
+
+    private static SystemTextJsonContentSerializer GetContentSerializer() =>
+        new(new JsonSerializerOptions
+        {
+            TypeInfoResolver = HomeAssistantSerializerContext.Default
+        });
 }
