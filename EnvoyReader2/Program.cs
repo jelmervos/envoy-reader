7af37a10563af453b95e47b43107ce870cb52ca0
Jelmer Vos <43109366+jelmervos@users.noreply.github.com>
2026-09-11 13:23:34 +0200

    Support for appsettings.json, env vars and user secrets

--- EnvoyReader2/Program.cs
+++ EnvoyReader2/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -18,6 +19,19 @@ using var host = Host.CreateDefaultBuilder(args)
             .AddTransient<IOutputWriter, PvOutputWriter>()
             .AddTransient<IPipeline, Pipeline>()
             .AddHostedService<EnvoyReaderService>();
+    })
+    .ConfigureAppConfiguration((hostingContext, config) =>
+    {
+        var env = hostingContext.HostingEnvironment;
+        config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+              .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: false);
+
+        config.AddEnvironmentVariables("ENVOYREADER_");
+
+        if (hostingContext.HostingEnvironment.IsDevelopment())
+        {
+            config.AddUserSecrets<Program>();
+        }
     })
     .Build();
 
